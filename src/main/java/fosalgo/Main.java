2179b88fb115097b545d962537d04d67b67b33c5
MAHMUD <72733031+mahmudhardy@users.noreply.github.com>
2021-03-03 21:27:21 +0700

    Update Main.java

--- src/main/java/fosalgo/Main.java
+++ src/main/java/fosalgo/Main.java
@@ -14,8 +14,92 @@ public class Main {
         
         //TULISKAN KODEMU DIBAWAH INI
         
-        
-        
+        System.out.println("Masukkan Bulan");
+        int bulan;
+        bulan = input.nextInt();
+        if (bulan == 1)
+        {
+            System.out.println("Januari");
+            System.out.println("Februari");
+            System.out.println("Maret");
+            System.out.println("April");
+        }
+        if (bulan == 2)
+        {
+            System.out.println("Februari");
+            System.out.println("Maret");
+            System.out.println("April");
+            System.out.println("Mei");
+        }
+        if (bulan == 3)
+        {
+            System.out.println("Maret");
+            System.out.println("April");
+            System.out.println("Mei");
+            System.out.println("Juni");
+        }
+        if (bulan == 4)
+        {
+            System.out.println("April");
+            System.out.println("Mei");
+            System.out.println("Juni");
+            System.out.println("Juli");
+        }
+        if (bulan == 5)
+        {
+            System.out.println("Mei");
+            System.out.println("Juni");
+            System.out.println("Juli");
+            System.out.println("Agustus");
+        }
+        if (bulan == 6)
+        {
+            System.out.println("Juni");
+            System.out.println("Juli");
+            System.out.println("Agustus");
+            System.out.println("September");
+        }
+        if (bulan == 7)
+        {
+            System.out.println("Juli");
+            System.out.println("Agustus");
+            System.out.println("September");
+            System.out.println("Oktober");
+        }
+        if (bulan == 8)
+        {
+            System.out.println("Agustus");
+            System.out.println("September");
+            System.out.println("Oktober");
+            System.out.println("November");
+        }
+        if (bulan == 9)
+        {
+            System.out.println("September");
+            System.out.println("Oktober");
+            System.out.println("November");
+            System.out.println("Desember");
+        }
+        if (bulan == 10)
+        {
+            System.out.println("Oktober");
+            System.out.println("November");
+            System.out.println("Desember");
+            System.out.println("Januari");
+        }
+        if (bulan == 11)
+        {
+            System.out.println("November");
+            System.out.println("Desember");
+            System.out.println("Januari");
+            System.out.println("Februari");
+        }
+        if (bulan == 12)
+        {
+            System.out.println("Desember");
+            System.out.println("Januari");
+            System.out.println("Februari");
+            System.out.println("Maret");
+        }
     }
-
 }
